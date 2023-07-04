cc680e67ddba4ca9fa17cd0ce04f598deaff8237
yudahan <103639913+danbi22@users.noreply.github.com>
2023-07-04 16:16:27 +0900

    py01_intro

--- py01_intro.py
+++ py01_intro.py
@@ -1,2 +1,15 @@
 # 파이썬 소개
-# inline
+# inline comment(인라인 주석)
+print('안녕하세요, Python!') # 파이썬 문장은 세미콜론을 사용하지 않음.
+
+# print() 함수
+# print() 함수에 아규먼트를 여러 개 전달하면, 아규먼트들을 공백으로 연결해서 콘솔에 출력함.
+print('hello', 'python', '안녕하세요')
+
+# format() 메서드를 사용한 출력
+print('{} + {} = {}'.format(1, 2, 1+2))
+
+# formatted string을 사용한 출력 Python 3.6 이상의 버전에서 사용할 수 있음.
+x = 1
+y = 2
+print(f'{x} + {y} = {x + y}')
